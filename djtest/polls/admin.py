0fe18002f6d822898da20d60fca8e86326bd47f2
Stanisław Drozd <drozdziak1@gmail.com>
2015-02-05 21:32:12 +0100

    set vagrant up

--- djtest/polls/admin.py
+++ djtest/polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from polls.models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
