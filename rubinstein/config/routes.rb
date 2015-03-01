0a57d754b06c126dff5a44e08e30995712d17981
Stanisław Drozd <drozdziak1@gmail.com>
2015-03-02 00:47:18 +0100

    stuff

--- rubinstein/config/routes.rb
+++ rubinstein/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'welcome/index'
 
-	get 'articles/new'
+	resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-	root 'home#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
